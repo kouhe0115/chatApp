85d804b25128050372d539c087848f0829a322d4
kou <>
2019-12-20 00:30:39 +0900

    chat画面のview実装

---
+++ resources/views/User/index.blade.php
@@ -0,0 +1,200 @@
+@extends('common.user')
+
+@section('content')
+
+    <nav class="navbar navbar-fixed-left">
+        <div class="container">
+            <div class="navbar-header navbar-left-header">GROUP</div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav-left-list">
+                    <li>
+                        <a href=""><i class="fa fa-briefcase">グループ１</i></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-pencil-square-o">グループ２</i></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-comments-o">グループ３</i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="chat__area">
+        <div class="chat__area__header">
+            <p class="chat__area__header--group__name">Group Name : </p>
+            <p class="chat__area__header--group__member">Group Member : </p>
+        </div>
+
+        <div class="chat__area__content">
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa actor__user">
+                <figure class="kaiwa-img-right">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323439" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-left">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa actor__user">
+                <figure class="kaiwa-img-right">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323439" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                        名前
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-left">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa actor__user">
+                <figure class="kaiwa-img-right">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323439" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                        名前
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-left">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa actor__user">
+                <figure class="kaiwa-img-right">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323439" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                        名前
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-left">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+            <div class="kaiwa auth__user">
+                <figure class="kaiwa-img-left">
+                    <img src="https://fotopus.com/photos/hd.php?cd=3323698" alt="no-img2">
+                    <figcaption class="kaiwa-img-description">
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        /////////チャットの内容////////////
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="chat__area__input__area">
+            <form class="chat__form" action="">
+                <input class="chat__form--input" type="text" name="" id="" placeholder="メッセージ入力">
+                <button class="chat__form--button" type="submit">送信</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/common/user.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'greenhorn_works') }}</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href="{{ asset('css/navbar-fixed-left.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/chat.css') }}" rel="stylesheet">
+    <!-- Scripts -->
+</head>
+
+<body>
+<nav class="navbar navbar-default navbar-fixed-top under-shadow">
+    <div class="container">
+        <div class="navbar-header">
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ route('home') }}">
+                CHAT APP
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Right Side Of Navbar -->
+            <div class="nav navbar-nav navbar-right nav-user">
+                <div class="dropdown">
+                    <a class="user-name-box dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{--                        <img src="{{ Auth::user()->avatar }}">&nbsp;&nbsp;&nbsp;{{ Auth::user()->name }}--}}
+                        画像&user_name
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+
+@yield('content')
+
+<!-- Scripts -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.0.0/moment.min.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
